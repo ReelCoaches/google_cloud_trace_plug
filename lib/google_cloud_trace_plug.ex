d5ea8cc33b75a0fb4388c5c127f8026044963344
Scott Bennett <scott@fantag.live>
2020-08-11 15:38:58 -0700

    implement plug

--- lib/google_cloud_trace_plug.ex
+++ lib/google_cloud_trace_plug.ex
@@ -1,18 +1,96 @@
 defmodule GoogleCloudTracePlug do
   @moduledoc """
-  Documentation for `GoogleCloudTracePlug`.
+  A plug for generating a unique trace context for each request.
+
+  The generated trace id will be a 32-character hexadecimal encoding of an
+  16-byte array, such as `4bf92f3577b34da6a3ce929d0e0e4736`.
+
+  The generated trace context will be  a 16-character hexadecimal encoding of an
+  8-byte array, such as `00f067aa0ba902b7`.
+
+  The trace option is either 0 or 1 depending on whether this reuqest should be
+  traced or not.
+
+  If a trace context already exists as the "X-Cloud-Trace-Context" HTTP request header,
+  then that value will be used.
   """
 
-  @doc """
-  Hello world.
+  require Logger
+  alias Plug.Conn
+  @behaviour Plug
 
-  ## Examples
+  @impl Plug
+  @spec init(keyword) :: keyword
+  def init(opts), do: opts
 
-      iex> GoogleCloudTracePlug.hello()
-      :world
+  @impl Plug
+  @spec call(Plug.Conn.t(), keyword) :: Plug.Conn.t()
+  def call(conn, opts) do
+    trace_context_header = Keyword.get(opts, :trace_context_header, "x-cloud-trace-context")
+    project_id = Keyword.get(opts, :project_id, "my-project")
 
-  """
-  def hello do
-    :world
+    conn
+    |> get_trace_context(trace_context_header)
+    |> set_trace_context(trace_context_header, project_id)
   end
+
+  defp get_trace_context(conn, header) do
+    case Conn.get_req_header(conn, header) do
+      [] -> {conn, {generate_trace_id(), generate_span_id(), should_trace?()}}
+      [val | _] -> {conn, parse_trace_header(val)}
+    end
+  end
+
+  defp set_trace_context({conn, {trace_id, span_id, should_trace?}}, header, project_id) do
+    # Log Entry trace format: projects/my-projectid/traces/06796866738c859f2f19b7cfb3214824
+    trace = "projects/#{project_id}/traces/#{trace_id}"
+
+    # Special Fields: https://cloud.google.com/logging/docs/agent/configuration
+    Logger.metadata("logging.googleapis.com/trace": trace)
+    Logger.metadata("logging.googleapis.com/spanId": span_id)
+    Logger.metadata("logging.googleapis.com/trace_sampled": parse_boolean(should_trace?))
+
+    # Cloud Trace context header format: TRACE_ID/SPAN_ID;o=TRACE_TRUE
+    Conn.put_resp_header(conn, header, "#{trace_id}/#{span_id};o=#{should_trace?}")
+  end
+
+  defp generate_trace_id do
+    generate_hex_bytes(16)
+  end
+
+  defp generate_span_id do
+    generate_hex_bytes(8)
+  end
+
+  defp generate_hex_bytes(num_bytes) do
+    num_bytes
+    |> :crypto.strong_rand_bytes()
+    |> :binary.decode_unsigned()
+    |> Integer.to_string(16)
+    |> String.downcase()
+  end
+
+  defp parse_trace_header(header) do
+    # Cloud Trace context header format: TRACE_ID/SPAN_ID;o=TRACE_TRUE
+    # Where:
+    #
+    # TRACE_ID is a 32-character hexadecimal value representing a 128-bit number.
+    # SPAN_ID is the decimal representation of the (unsigned) span ID.
+    # TRACE_TRUE must be 1 to trace this request. Specify 0 to not trace the request.
+    [trace_id, rest] = String.split(header, "/")
+
+    if String.contains?(rest, ";") do
+      [span_id, options] = String.split(rest, ";")
+      [_, should_trace?] = String.split(options, "=")
+
+      {trace_id, span_id, should_trace?}
+    else
+      {trace_id, rest, "0"}
+    end
+  end
+
+  defp should_trace?, do: "0"
+
+  defp parse_boolean("1"), do: true
+  defp parse_boolean("0"), do: false
 end
--- test/google_cloud_trace_plug_test.exs
+++ test/google_cloud_trace_plug_test.exs
@@ -1,8 +1,76 @@
 defmodule GoogleCloudTracePlugTest do
-  use ExUnit.Case
-  doctest GoogleCloudTracePlug
+  use ExUnit.Case, async: true
+  use Plug.Test
 
-  test "greets the world" do
-    assert GoogleCloudTracePlug.hello() == :world
+  defp call(conn, opts) do
+    GoogleCloudTracePlug.call(conn, GoogleCloudTracePlug.init(opts))
+  end
+
+  test "generates new trace context if none exists" do
+    conn = call(conn(:get, "/"), [])
+    [res_trace_context] = get_resp_header(conn, "x-cloud-trace-context")
+
+    assert meta_trace = Logger.metadata()[:"logging.googleapis.com/trace"]
+    {_project_id, trace_id} = parse_trace(meta_trace)
+
+    assert meta_span_id = Logger.metadata()[:"logging.googleapis.com/spanId"]
+
+    assert res_trace_context =~ trace_id
+    assert res_trace_context =~ meta_span_id
+  end
+
+  test "uses existing trace context" do
+    trace_id = "4f535edc4efa0e8ac7394c58d0e2acf8"
+    span_id = "6600be06d0b2b630"
+
+    conn =
+      conn(:get, "/")
+      |> put_req_header("x-cloud-trace-context", "#{trace_id}/#{span_id};o=0")
+      |> call([])
+
+    [res_trace_context] = get_resp_header(conn, "x-cloud-trace-context")
+    meta_trace = Logger.metadata()[:"logging.googleapis.com/trace"]
+    meta_span_id = Logger.metadata()[:"logging.googleapis.com/spanId"]
+
+    assert meta_trace =~ trace_id
+    assert meta_span_id =~ span_id
+
+    assert res_trace_context =~ trace_id
+    assert res_trace_context =~ span_id
+  end
+
+  test "uses existing trace context without options" do
+    trace_id = "4f535edc4efa0e8ac7394c58d0e2acf8"
+    span_id = "6600be06d0b2b630"
+
+    conn =
+      conn(:get, "/")
+      |> put_req_header("x-cloud-trace-context", "#{trace_id}/#{span_id}")
+      |> call([])
+
+    [res_trace_context] = get_resp_header(conn, "x-cloud-trace-context")
+    meta_trace = Logger.metadata()[:"logging.googleapis.com/trace"]
+    meta_span_id = Logger.metadata()[:"logging.googleapis.com/spanId"]
+
+    assert meta_trace =~ trace_id
+    assert meta_span_id =~ span_id
+
+    assert res_trace_context =~ trace_id
+    assert res_trace_context =~ span_id
+  end
+
+  test "uses project id passed in as option" do
+    project_id = "fantag-c0979"
+
+    call(conn(:get, "/"), project_id: project_id)
+
+    meta_trace = Logger.metadata()[:"logging.googleapis.com/trace"]
+
+    assert meta_trace =~ project_id
+  end
+
+  defp parse_trace(trace) do
+    ["projects", project_id, "traces", trace_id] = String.split(trace, "/")
+    {project_id, trace_id}
   end
 end
